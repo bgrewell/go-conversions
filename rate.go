9089926c8cb01ac119e52bbd8332f4b0518a5be1
Ben Grewell <bgrewell@gmail.com>
2020-12-08 14:26:54 -0800

    update rate conversions

--- rate.go
+++ rate.go
@@ -25,12 +25,16 @@ func ConvertStringToUInt64orNil(uintStr string) *uint64 {
 	return &value
 }
 
+func ConvertStringToUint64or0(uintStr string) uint64 {
+	value := ConvertStringToUInt64orNil(uintStr)
+	if value == nil {
+		return 0
+	}
+	return *value
+}
+
 func ConvertToRateMbps(lastBytes uint64, currentBytes uint64, lastTime int64, currentTime int64) float32 {
-	period := float32(currentTime - lastTime) / float32(int64(time.Second))
-	change := (currentBytes - lastBytes) * 8
-	rate := float32(change) / period
-	mbps := rate / 1000 / 1000
-	return mbps
+	return convertByteCountsToRate(lastBytes, currentBytes, lastTime, currentTime, PREFIX_MEGABITS)
 }
 
 func convertByteCountsToRate(lastBytes uint64, currentBytes uint64, lastTime int64, currentTime int64, units BinaryPrefix) float32 {
