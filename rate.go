82181d5fd43a605ce50a68259860d36c7bcf820a
Ben Grewell <bgrewell@gmail.com>
2020-12-08 14:22:21 -0800

    add rate conversions

---
+++ rate.go
@@ -0,0 +1,41 @@
+package conversions
+
+import (
+	"strconv"
+	"time"
+)
+
+type BinaryPrefix int
+
+const (
+	PREFIX_BITS BinaryPrefix = 1
+	PREFIX_KILOBITS BinaryPrefix = 1 * 1000
+	PREFIX_MEGABITS BinaryPrefix = 1 * 1000 * 1000
+	PREFIX_GIGABITS BinaryPrefix = 1 * 1000 * 1000 * 1000
+	PREFIX_TERABITS BinaryPrefix = 1 * 1000 * 1000 * 1000 * 1000
+
+	BITS_PER_BYTE = 8
+)
+
+func ConvertStringToUInt64orNil(uintStr string) *uint64 {
+	value, err := strconv.ParseUint(uintStr, 10, 64)
+	if err != nil {
+		return nil
+	}
+	return &value
+}
+
+func ConvertToRateMbps(lastBytes uint64, currentBytes uint64, lastTime int64, currentTime int64) float32 {
+	period := float32(currentTime - lastTime) / float32(int64(time.Second))
+	change := (currentBytes - lastBytes) * 8
+	rate := float32(change) / period
+	mbps := rate / 1000 / 1000
+	return mbps
+}
+
+func convertByteCountsToRate(lastBytes uint64, currentBytes uint64, lastTime int64, currentTime int64, units BinaryPrefix) float32 {
+	period := float32(currentTime - lastTime) / float32(int64(time.Second))
+	change := (currentBytes - lastBytes) * BITS_PER_BYTE
+	rate := float32(change) / period
+	return rate / float32(units)
+}
